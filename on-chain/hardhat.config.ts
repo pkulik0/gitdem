101ea1e0e032975762a27a8b33c7314f44eedf21
pkulik0 <me@pkulik.com>
2025-04-14 13:25:33 +0800

    on-chain: add placeholder pk for sepolia

--- on-chain/hardhat.config.ts
+++ on-chain/hardhat.config.ts
@@ -1,7 +1,8 @@
 import { HardhatUserConfig, vars } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 
-const SEPOLIA_PRIVATE_KEY = vars.get("SEPOLIA_PRIVATE_KEY", "");
+// The default is hardhat's test account #0 - just to silence errors in CI
+const SEPOLIA_PRIVATE_KEY = vars.get("SEPOLIA_PRIVATE_KEY", "0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80");
 const INFURA_API_KEY = vars.get("INFURA_API_KEY", "");
 
 const config: HardhatUserConfig = {
