6d2f44a241e17ba28db4efdf2088e3dd34f0a6f5
pkulik0 <me@pkulik.com>
2025-04-14 13:08:37 +0800

    on-chain: add sepolia to networks

--- on-chain/hardhat.config.ts
+++ on-chain/hardhat.config.ts
@@ -1,8 +1,16 @@
-import { HardhatUserConfig } from "hardhat/config";
+import { HardhatUserConfig, vars } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 
+const SEPOLIA_PRIVATE_KEY = vars.get("SEPOLIA_PRIVATE_KEY");
+const INFURA_API_KEY = vars.get("INFURA_API_KEY");
 const config: HardhatUserConfig = {
   solidity: "0.8.28",
+  networks: {
+    sepolia: {
+      url: `https://sepolia.infura.io/v3/${INFURA_API_KEY}`,
+      accounts: [SEPOLIA_PRIVATE_KEY],
+    }
+  }
 };
 
 export default config;
