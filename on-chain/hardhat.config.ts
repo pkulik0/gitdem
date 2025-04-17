e1ba038966972502093c78c695583eac995eb261
pkulik0 <me@pkulik.com>
2025-04-17 14:49:48 +0900

    on-chain: add contract code verification for avax

--- on-chain/hardhat.config.ts
+++ on-chain/hardhat.config.ts
@@ -1,18 +1,42 @@
 import { HardhatUserConfig, vars } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import "@nomicfoundation/hardhat-verify";
 
 // The default is hardhat's test account #0 - just to silence errors in CI
-const SEPOLIA_PRIVATE_KEY = vars.get("SEPOLIA_PRIVATE_KEY", "0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80");
+const PRIVATE_KEY = vars.get("PRIVATE_KEY", "0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80");
 const INFURA_API_KEY = vars.get("INFURA_API_KEY", "");
 
 const config: HardhatUserConfig = {
   solidity: "0.8.28",
+  etherscan: {
+    apiKey: {
+      avax_mainnet: "not-required",
+    },
+    customChains: [
+      {
+        network: "avax_mainnet",
+        chainId: 43114,
+        urls: {
+          apiURL: "https://api.routescan.io/v2/network/mainnet/evm/43114/etherscan",
+          browserURL: "https://avalanche.routescan.io"
+        }
+      }
+    ]
+  },
   networks: {
     sepolia: {
       url: `https://sepolia.infura.io/v3/${INFURA_API_KEY}`,
-      accounts: [SEPOLIA_PRIVATE_KEY],
-    }
-  }
+      accounts: [PRIVATE_KEY],
+    },
+    avax_fuji: {
+      url: `https://avalanche-fuji.infura.io/v3/${INFURA_API_KEY}`,
+      accounts: [PRIVATE_KEY],
+    },
+    avax_mainnet: {
+      url: `https://avalanche-mainnet.infura.io/v3/${INFURA_API_KEY}`,
+      accounts: [PRIVATE_KEY],
+    },
+  },
 };
 
 export default config;
